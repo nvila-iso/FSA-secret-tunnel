502fa8a2e3f8ed8189f5bbebc00e1066e5acedb8
nvila-iso <nvila88@proton.me>
2025-08-15 17:58:39 -0400

    added incomplete api call / entrance now takes a user entered name

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,18 +1,50 @@
 import { createContext, useContext, useState } from "react";
 
 const API = "https://fsa-jwt-practice.herokuapp.com";
+const register = API + "/signup"
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [token, setToken] = useState();
   const [location, setLocation] = useState("GATE");
+  const [userName, setUserName] = useState();
+
 
   // TODO: signup
+  const userSignup = async () => {
+    try {
+      const result = await fetch (register, {
+        method : "POST", 
+        headers: {
+          "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({
+          username: {userName}
+        })
+      })
+      
+    } catch (error) {
+      
+    }
+  }
+
+
+  // fetch(API + "/signup",
+  //             {
+  //               method: "POST",
+  //               headers: {
+  //                 "Content-Type": "application/json"
+  //               },
+  //               body: JSON.stringify({
+  //                 username: (userName),
+  //                 // password: "super-secret-999"
+  //               })
+  //             })
 
   // TODO: authenticate
 
-  const value = { location };
+  const value = { location, setUserName };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
